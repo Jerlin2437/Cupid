d4f61ad904e8b11cc7f0508c30f8e81516da4f18
ryanchwang <ryanchwang@gmail.com>
2024-05-18 00:51:42 -0400

    njnj

--- lib/src/login_screen.dart
+++ lib/src/login_screen.dart
@@ -12,7 +12,7 @@ class LoginScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             const Text(
-              'Cupid',
+              'CupidTest',
               style: TextStyle(
                 fontSize: 32.0,
                 fontWeight: FontWeight.bold,
